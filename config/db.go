2efcb6e1aa8d1b4284711a35034e2155fcca771b
Lincon Avila <lincon1000ls@gmail.com>
2025-06-16 23:09:11 -0300

    Add database

--- config/db.go
+++ config/db.go
@@ -3,6 +3,7 @@ package config
 import (
 	"database/sql"
 	"log"
+	"os"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -13,9 +14,26 @@ func SetupDatabase() {
 	}
 	defer db.Close()
 
+	migrations := []string{
+		"./migrations/001_create_users.sql",
+		"./migrations/002_create_bets.sql",
+		"./migrations/003_create_games.sql",
+	}
+
+	for _, migrationFile := range migrations {
+		migration, err := os.ReadFile(migrationFile)
+		if err != nil {
+			log.Fatalf("Erro ao ler o arquivo de migração %s: %v", migrationFile, err)
+		}
+		_, err = db.Exec(string(migration))
+		if err != nil {
+			log.Fatalf("Erro ao executar a migração %s: %v", migrationFile, err)
+		}
+	}
+
 	err = db.Ping()
 	if err != nil {
 		log.Fatal("Erro ao conectar ao banco de dados:", err)
 	}
-	log.Println("Banco de dados conectado com sucesso!")
+	log.Println("Migrações concluídas e banco de dados conectado com sucesso.")
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,7 @@ module berry_bet
 go 1.24.3
 
 require github.com/mattn/go-sqlite3 v1.14.28
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/gorilla/mux v1.8.1 
