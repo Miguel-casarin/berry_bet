cbe4d67e223572b7fd6e23b041ae925b55de5537
Lincon Avila <lincon1000ls@gmail.com>
2025-06-20 14:43:35 -0300

    feat(auth): adiciona registro de usuário e busca por username/email

--- api/auth/auth.go
+++ api/auth/auth.go
@@ -2,9 +2,11 @@ package auth
 
 import (
 	"berry_bet/internal/auth"
-    "github.com/gin-gonic/gin"
+
+	"github.com/gin-gonic/gin"
 )
 
 func RegisterAuthRoutes(router *gin.Engine) {
-    router.POST("/login", auth.LoginHandler)
+	router.POST("/login", auth.LoginHandler)
+	router.POST("/register", auth.RegisterHandler)
 }
