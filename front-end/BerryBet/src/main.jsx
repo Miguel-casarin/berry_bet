3acd22999160f1cc061991bd0c9c0c580f3c59cb
unknown <pietronoblemarques@gmail.com>
2025-06-28 03:58:19 -0300

    Adicionando algumas alterações massivas de react

--- front-end/BerryBet/src/main.jsx
+++ front-end/BerryBet/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Dashboard from './pages/dashboard'
-import JogoDoTigrinho from './pages/jogodoTigrinho'
+import JogoDoTigrinho from './jogofuncional/jogodoTigrinho'
 import Deposito from './pages/deposito'
+import ApostaEsportiva from './pages/apostaEsportiva'
 import './index.css'
+import Saque from './pages/saque'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -15,7 +17,10 @@ createRoot(document.getElementById('root')).render(
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/jogodoTigrinho" element={<JogoDoTigrinho />} />
         <Route path="/deposito" element={<Deposito />} />
+        <Route path="/apostaEsportiva" element={<ApostaEsportiva />} />
+        <Route path="/saque" element={Saque} />
       </Routes>
+
     </BrowserRouter>
   </StrictMode>,
 )
---
+++ front-end/BerryBet/src/pages/apostaEsportiva.jsx
@@ -0,0 +1,11 @@
+function ApostaEsportiva() {
+  return (
+    <div>
+      <h1>Aposta Esportiva</h1>
+      <p>Bem-vindo à página de Aposta Esportiva!</p>
+      <p>Aqui você pode encontrar informações sobre apostas em eventos esportivos.</p>
+    </div>
+  );
+}   
+
+export default ApostaEsportiva;
