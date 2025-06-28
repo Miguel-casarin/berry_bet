ad062499f3f48e294d9bd4387c7d7123fc738c25
Miguel-casarin <casarindasilvamiguel@gmail.com>
2025-06-28 16:53:36 -0300

    função de valor para aposta e controle dos mecanismos de sorteio

--- internal/games/roleta/roleta.go
+++ internal/games/roleta/roleta.go
@@ -26,6 +26,7 @@ func Get_Saldo_Atual(userID int64) (float64, error) {
 	return user_stats.GetUserBalance(userID)
 }
 
+// rever
 func Discount(userID int64, value float64) (float64, error) {
 	saldo, err := user_stats.GetUserBalance(userID)
 	if err != nil {
@@ -60,10 +61,11 @@ func (d Dados_rodadas) CartinhaSorteada() *string {
 	return &c
 }
 
-func Start(saldo_aposta float64) float64 {
-	saldo_aposta = saldo_aposta + Randon_inicial(saldo_aposta)
+func Start(valor_aposta float64) float64 {
+	valor_aposta = valor_aposta + Randon_inicial(valor_aposta)
 	_ = Update_wins_losses(userID, true)
-	return saldo_aposta
+	user_stats.IncrementUserTotalBets(userID)
+	return valor_aposta
 }
 
 // criar uma struct para guardar os resultados
@@ -77,9 +79,9 @@ type Dados_rodadas struct {
 	cartinha_sorteada *cartinha
 }
 
-func Final(saldo_aposta float64) Dados_rodadas {
+func Final(valor_aposta float64) Dados_rodadas {
 	data := Dados_rodadas{
-		valor_aposta:      saldo_aposta,
+		valor_aposta:      valor_aposta,
 		historical_value:  0,
 		loser_count:       0,
 		statistical_loser: 0,
@@ -96,6 +98,7 @@ func Final(saldo_aposta float64) Dados_rodadas {
 		data.cartinha_sorteada = carta
 
 		_ = Update_wins_losses(userID, true)
+		user_stats.IncrementUserTotalBets(userID)
 	} else {
 		data.valor_aposta = 0
 		data.historical_value = data.historical_value + Count_money(data.historical_value, data.valor_aposta)
@@ -104,20 +107,24 @@ func Final(saldo_aposta float64) Dados_rodadas {
 		data.cartinha_sorteada = nil
 
 		_ = Update_wins_losses(userID, false)
+		user_stats.IncrementUserTotalBets(userID)
 	}
 
 	if data.loser_count == 3 {
 		data.valor_aposta = Give_low(data.valor_aposta)
 		data.statistical_loser = data.statistical_loser + Statistical_loser(data.loser_count)
 		data.loser_count = 0
+		user_stats.IncrementUserTotalBets(userID)
 	}
 
 	if data.historical_value >= data.limit {
 		Governo(data.valor_aposta)
+		user_stats.IncrementUserTotalBets(userID)
 	}
 
 	if data.victory >= 5 {
 		Governo(data.valor_aposta)
+		user_stats.IncrementUserTotalBets(userID)
 	}
 
 	return data
